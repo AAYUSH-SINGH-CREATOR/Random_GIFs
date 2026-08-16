8520f6db70c936c4d33f6086c7a1d5dacc223604
Aayush Singh Chauhan <1211aayushh@gmail.com>
2026-08-17 01:08:45 +0530

    spinner solved 2.0

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -12,9 +12,11 @@ export default function Tag() {
     return (
         <div className="flex flex-col bg-pink-500 w-9/12 items-center p-3 mb-10">
             <h1 className="text-2xl text-center font-bold text-black mb-2 underline uppercase">Random {tag} gifs</h1>
-            {
+          <div className=" min-h-50 min-w-100 flex justify-center items-center"> 
+             {
                 loading ? <Spinner /> : <img src={gif} alt="" />
             }
+            </div>
             <input type="text" placeholder="Enter tag" className="bg-white text-black mt-3 rounded-md p-1 w-9/12" value={tag} onChange={(e) => settag(e.target.value)} />
             <button onClick={gifhandler} className=" bg-amber-300  text-black w-9/12 rounded-md p-2 font-medium text-xl border border-black border-t-transparent">Generate</button>
         </div>
