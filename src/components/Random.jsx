597d06f41f9539b902d7ff0d5adcf86658e1e0c8
Aayush Singh Chauhan <1211aayushh@gmail.com>
2026-08-17 01:06:19 +0530

    spinner flow solved

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -8,12 +8,16 @@ function Random() {
     const{gif, loading, gifhandler} = useGif();
 
     return (
-        <div className="bg-green-500 w-9/12 flex flex-col items-center p-3">
+        <div className="bg-green-500 w-9/12 flex flex-col items-center p-3 ">
             <h1 className="text-2xl font-bold text-black mb-2 underline uppercase">A random GIFS</h1>
-            
+         
+            <div className=" min-h-50 min-w-100 flex justify-center items-center" >
+   
             {
-                loading? <Spinner/> :  <img src={gif} alt="" />
+                loading? <Spinner/> :  <img src={gif} alt=""/>
             }
+
+       </div>
 
             <img src="loading? setloading(false) : {gif} " alt="" />
             <button onClick={gifhandler} className="mt-3 bg-amber-300  text-black w-9/12 rounded-md p-2 font-medium text-xl border border-black ">
